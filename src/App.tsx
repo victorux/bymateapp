5c55260c575fdd5b9db2f11599642c916cd8a2db
victorb <hi.victorb@gmail.com>
2024-02-15 12:32:10 +0000

    added colors pallete

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-xl font-boldmt-40'>Hello World!</h1>
+     <h1 className='text-black'>Hello World!</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,38 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        50: '#FFEDF0',
+        75: '#FFB3C1',
+        100: '#FF94A7',
+        200: '#FF6581',
+        300: '#FF4667',
+        400: '#B33148',
+        500: '#9C2B3F',
+      },
+      secondary: {
+        50: '#EBF9F6',
+        75: '#ACE8DA',
+        100: '#8ADFCA',
+        200: '#8ADFCA',
+        300: '#35C7A4',
+        400: '#258B73',
+        500: '#207964',
+      },
+      neutral: {
+        50: '#FCFCFC',
+        75: '#F1F2F4',
+        100: '#ECECEF',
+        200: '#E4E4E8',
+        300: '#DEDFE3',
+        400: '#9B9C9F',
+        500: '#87888A',
+      },
+      black: '#181B2A',
+      white: '#FFFFFF'
+    },
+  extend: {},
   },
   plugins: [],
 }
