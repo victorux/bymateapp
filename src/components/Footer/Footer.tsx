aff748769c7af6ecf626f36a10744410ea627824
victorb <hi.victorb@gmail.com>
2024-02-20 14:44:22 +0000

    footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -17,8 +17,15 @@ const Footer = () => {
 
   return (
     <footer className={`absolute bottom-0 left-0 right-0 bg-neutral-75 py-6 ${isContentLong ? 'relative' : 'fixed'}`}>
-      <div className='container'>
-        This is footer.
+      <div className='container flex justify-between'>
+        <div className='flex gap-8'>
+          <a href='#'>support@bymate.com</a>
+          <a href='#'>Privacy & Terms</a>
+          <a href='#'>Testimonials</a>
+          <a href='#'>Sitemap</a>
+        </div>
+        
+        <span>© 2024 bymate LTD.</span>
       </div>
     </footer>
   );
