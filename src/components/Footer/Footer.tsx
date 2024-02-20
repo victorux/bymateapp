4ec7129687c8135613ddcc552679229e6ba6b4d5
victorb <hi.victorb@gmail.com>
2024-02-20 14:35:46 +0000

    Footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
 
+const Footer = () => {
+  const [isContentLong, setIsContentLong] = useState(false);
 
-export default function Footer() {
+  useEffect(() => {
+    const handleResize = () => {
+      setIsContentLong(window.innerHeight < document.body.scrollHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Check initially
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={`absolute bottom-0 left-0 right-0 bg-neutral-75 py-6 ${isContentLong ? 'relative' : 'fixed'}`}>
+      <div className='container'>
+        This is footer.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
