d050e2ba363e5eadf5ef2ad2ad9976af1365a955
victorb <hi.victorb@gmail.com>
2024-02-21 14:28:01 +0000

    react-router-dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
+import ErrorPage from "./pages/error-page";
+import Home from "./pages/home/Home"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+]);
+
 
 function App() {
   return (
     <>
     <Header />
     <body>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, iste, provident maiores eveniet, ad est nulla neque dolore maxime quaerat voluptas ducimus? Asperiores rerum fugiat, quae excepturi officia ipsa quisquam.
-      Ea nemo eligendi, non omnis voluptate rerum aut culpa unde mollitia nostrum sed autem rem quasi eius blanditiis illo excepturi laudantium sint? Aperiam, nisi consequatur ipsum doloribus unde quo non!
-      Dolore cum consectetur obcaecati adipisci dolorem repellat eum quod, quidem voluptatibus culpa et repellendus. Quasi, eius reiciendis ipsam temporibus sequi, molestiae voluptatum autem ut amet delectus tenetur ea facilis reprehenderit?
-      Dolorem neque sed magnam rerum fuga quibusdam laborum dicta. Provident, corrupti. Laudantium facere fuga et fugiat neque magnam cum ullam distinctio quibusdam. Dolore vero molestias numquam dolorum consequuntur veniam suscipit.
-      Vitae itaque rerum neque amet, officia, cumque minus libero facere doloribus praesentium dolor quam accusantium soluta qui nihil repellat reprehenderit, aperiam ipsa odit eius. Totam nemo beatae in maiores commodi?
-      Expedita saepe est fugit, nihil consectetur alias odio. Soluta dicta ducimus obcaecati atque minima temporibus, reiciendis corporis recusandae iste magni laudantium accusamus fugiat ut dignissimos veritatis eos, molestiae quam labore.
-      Voluptatem cum id aperiam voluptatibus praesentium harum fugit repellat ipsam perspiciatis laboriosam aut, molestiae delectus fugiat, reprehenderit mollitia officiis quod odio libero molestias deserunt cupiditate cumque. Nam unde voluptatibus quam!
-      Nobis nisi fugiat minus maxime ipsum dolores! Repellat est quibusdam rem fugit porro accusamus debitis, minima corporis neque unde totam dignissimos sed dolorem perferendis repudiandae praesentium eos, provident modi eum?
-      Accusantium recusandae delectus at ipsam tempora, eligendi voluptas aliquam ut inventore minima molestias eveniet ea atque mollitia harum? Blanditiis aspernatur cupiditate itaque pariatur, velit facere voluptatum ipsam a in tenetur!
-      Autem cumque inventore necessitatibus? Consequatur nam est debitis nobis quam dolore quisquam doloremque laborum, quo esse quasi quia, dolores quos recusandae dolorum expedita eligendi quis? Harum nesciunt laudantium temporibus esse.
-      Eaque recusandae autem error quos cum. Facere deserunt dolore, ipsum voluptatibus officia consequatur itaque sed. Perferendis, recusandae veniam officiis ducimus deserunt facilis corporis, sed voluptatem repellat eum laudantium modi. Distinctio!
-      Ex quos cum culpa labore fugiat nam, quo ratione voluptatem corrupti rem dolorem alias illo dolore itaque error architecto non laboriosam praesentium at enim nemo a! Qui et error molestiae!
-      Dolore non natus, beatae explicabo amet sapiente hic. Doloremque officiis aperiam voluptatem repellendus ratione? Esse beatae ea atque placeat sit facere velit cupiditate dolor, accusamus, repellendus vel veniam numquam natus!
-      Harum obcaecati nesciunt laudantium? Ea minima dolorem magni ipsum facilis nisi ex suscipit rerum beatae ab commodi, sequi culpa error quae aspernatur velit nesciunt, possimus mollitia fugit delectus cum! Accusamus.
-      Laudantium suscipit veniam dolore expedita libero laboriosam autem impedit magni nobis debitis, iste maiores tenetur consequatur voluptatem distinctio? Nisi debitis magnam similique necessitatibus facere esse eaque facilis minima quia adipisci!
-      Asperiores tempora doloribus voluptate quam, molestias ea iure perferendis, nulla sint veritatis amet natus temporibus et reprehenderit. Beatae commodi, vitae, perspiciatis, neque nobis voluptatem sapiente est repudiandae distinctio cumque nemo.
-      Unde a recusandae minima quam tenetur, velit illum, corrupti illo, eveniet deserunt veritatis deleniti explicabo natus nostrum fugit! Odit pariatur qui, beatae facere nostrum mollitia blanditiis? Ea adipisci eos sequi.
-      Eum incidunt, eos quos neque odio perspiciatis harum consectetur ipsa. Deserunt recusandae animi temporibus numquam corrupti accusamus dignissimos! Magnam, non? In commodi quo iure laudantium exercitationem ipsam, distinctio rerum obcaecati.
-      Architecto accusamus accusantium explicabo fugiat, ipsum cumque amet et quo placeat ipsam, consectetur necessitatibus sequi nobis ullam at incidunt omnis rerum cum expedita. Repudiandae itaque architecto consequuntur doloribus officia nesciunt.
-      Eligendi facere, repellendus quos officiis neque ex qui quod veritatis tenetur blanditiis quis vero aliquam nesciunt dicta ducimus odit minima laborum fugit dolorum, consequuntur architecto! Nemo dolor quis numquam mollitia.
+      <RouterProvider router={router} />
     </body>
     <Footer />
     </>
