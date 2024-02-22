9fb7606d14b70164e5f19febff5f5172bd8a00ad
victorb <hi.victorb@gmail.com>
2024-02-22 14:49:15 +0000

    Hero Section

---
+++ src/pages/home/HeroSection.tsx
@@ -0,0 +1,30 @@
+import style from './home.module.scss';
+
+interface HeroSectionProps {
+    title: string;
+    subtitle: string;
+    image: string;
+    popularCities: { name: string, roomsLink: string, roomatesLink: string }[];
+}
+
+export default function HeroSection({ title, subtitle, image, popularCities }: HeroSectionProps) {
+    return (
+        <div className={style.heroSection} style={{ backgroundImage: `url(${image})` }}>
+            <div className="container">
+                <h1 className='text-white max-w-[1000px] mx-auto font-display font-bold'>{title}</h1>
+                <p className='text-white mt-4 font-body text-xl tracking-wide'>{subtitle}</p>
+                <div className='flex justify-center mt-12 gap-8 flex-wrap'>
+                    {
+                        popularCities.map((city, index) => (
+                            <div key={index} className='rounded-2xl border-opacity-50 px-8 py-4 flex flex-col gap-2 text-white backdrop-blur-md shadow-xl border-[1px] border-white border-opacity-20'>
+                                <h5 className='text-white'>{city.name}</h5>
+                                <a href={city.roomatesLink}>Rooms</a>
+                                <a href={city.roomatesLink}>Roommates</a>
+                            </div>
+                        )) 
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,35 @@
+import HeroSection from "./HeroSection";
+import bg from "../../assets/img/bg.jpg";
+
+const data = {
+  title: "Looking for a room or a roommate? We've got you covered!",
+  subtitle: "Explore affordable and convenient room rentals. Your ideal space is just a click away! ",
+  image: "https://img.freepik.com/premium-photo/3drendered-drawing-room-model-with-orange-color-theme_1066261-43119.jpg?w=1380",
+  popularCities: [
+    {
+      name: "Dublin",
+      roomsLink: "/rooms/dublin",
+      roomatesLink: "/roommates/dublin",
+    },
+    { name: "Limerick", 
+      roomsLink: "/rooms/limerick", 
+      roomatesLink: "/roommates/limerick", 
+    }, 
+    { name: "Galway", 
+      roomsLink: "/rooms/galway", 
+      roomatesLink: "/roommates/galway", 
+    }, 
+    { name: "Cork", 
+      roomsLink: "/rooms/cork", 
+      roomatesLink: "/roommates/cork", 
+    },
+  ]
+};
 
 export default function Home() {
   return (
-    <div>
-      <div className="container"></div>
-    </div>
+    <>
+      <HeroSection title={data.title} subtitle={data.subtitle} image={bg} popularCities={data.popularCities} />
+    </>
   )
 }
