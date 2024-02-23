b7b979a000b6c177a25a14b330ee29dfce404fc4
victorb <hi.victorb@gmail.com>
2024-02-23 13:03:50 +0000

    Header Styling

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -7,23 +7,12 @@ interface HeroSectionProps {
     popularCities: { name: string, roomsLink: string, roomatesLink: string }[];
 }
 
-export default function HeroSection({ title, subtitle, image, popularCities }: HeroSectionProps) {
+export default function HeroSection({ title, subtitle, image }: HeroSectionProps) {
     return (
-        <div className={style.heroSection} style={{ backgroundImage: `url(${image})` }}>
+        <div className={style.hero__section} style={{ backgroundImage: `url(${image})` }}>
             <div className="container">
-                <h1 className='text-white max-w-[1000px] mx-auto font-display font-bold'>{title}</h1>
-                <p className='text-white mt-4 font-body text-xl tracking-wide'>{subtitle}</p>
-                <div className='flex justify-center mt-12 gap-8 flex-wrap'>
-                    {
-                        popularCities.map((city, index) => (
-                            <div key={index} className='rounded-2xl border-opacity-50 px-8 py-4 flex flex-col gap-2 text-white backdrop-blur-md shadow-xl border-[1px] border-white'>
-                                <h5 className='text-white'>{city.name}</h5>
-                                <a href={city.roomatesLink}>Rooms</a>
-                                <a href={city.roomatesLink}>Roommates</a>
-                            </div>
-                        )) 
-                    }
-                </div>
+                <h1 className={style.hero__heading}>{title}</h1>
+                <p className={style.hero__body}>{subtitle}</p>
             </div>
         </div>
     );
