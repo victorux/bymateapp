262fcc37055872b247f4eb8843a8e4e8b1661f61
victorb <hi.victorb@gmail.com>
2024-03-05 11:55:49 +0000

    added prettier config

--- src/components/common/Buttons/Button.tsx
+++ src/components/common/Buttons/Button.tsx
@@ -1,36 +1,49 @@
-import { MouseEvent } from 'react';
+import { MouseEvent } from "react";
 
-import styles from './Button.module.scss';
+import styles from "./Button.module.scss";
 
 interface ButtonProps {
   children: string;
   onClick: (e: MouseEvent) => void;
-  size?: 'small' | 'medium' | 'large';
-  color?: 'primary' | 'secondary';
+  size?: "small" | "medium" | "large";
+  color?: "primary" | "secondary";
   disabled?: boolean;
   border?: boolean;
   icon?: React.ReactNode;
-  iconPosition?: 'left' | 'right';
+  iconPosition?: "left" | "right";
 }
 
 function Button({
-    children,
-    onClick,
-    size,
-    color,
-    disabled,
-    border,
-    icon,
-    iconPosition = 'left'
+  children,
+  onClick,
+  size,
+  color,
+  disabled,
+  border,
+  icon,
+  iconPosition = "left",
 }: ButtonProps) {
-    const classNames = `${styles.Button} ${styles[size || 'medium']} ${styles[color || 'transparent']} ${styles[border ? 'border' : 'no-border']} ${disabled ? styles.disabled : ''}`;
-    return (
-        <button type='button' className={classNames} onClick={onClick} disabled={disabled}>
-            {icon && iconPosition === 'left' && <span className={styles.icon}>{icon}</span>}
-            {children}
-            {icon && iconPosition === 'right' && <span className={styles.icon}>{icon}</span>}
-        </button>
-    )
+  const classNames = `${styles.Button} ${styles[size || "medium"]} ${
+    styles[color || "transparent"]
+  } ${styles[border ? "border" : "no-border"]} ${
+    disabled ? styles.disabled : ""
+  }`;
+  return (
+    <button
+      type="button"
+      className={classNames}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {icon && iconPosition === "left" && (
+        <span className={styles.icon}>{icon}</span>
+      )}
+      {children}
+      {icon && iconPosition === "right" && (
+        <span className={styles.icon}>{icon}</span>
+      )}
+    </button>
+  );
 }
 
 export default Button;
