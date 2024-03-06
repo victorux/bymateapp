17a5349b2b31740ca6449702f975b7d861ffce19
victorb <hi.victorb@gmail.com>
2024-03-06 14:25:19 +0000

    tested svgr stroke and fill

--- src/components/layout/Header/UserMenu.tsx
+++ src/components/layout/Header/UserMenu.tsx
@@ -1,10 +1,12 @@
 /// <reference types="vite-plugin-svgr/client" />
 import MenuIcon from '../../../assets/icons/menu.svg?react'
+import UserIcon from '../../../assets/icons/user.svg?react'
 
 export default function Menu() {
   return (
     <div>
-      <MenuIcon width={24} height={24} stroke="blue" />
+      <MenuIcon width={24} height={24} />
+      <UserIcon width={24} height={24} />
     </div>
   )
 }
