792c8e2385f240a4d7e3d2ff3b0f221608172aa5
victorb <hi.victorb@gmail.com>
2024-03-07 14:17:36 +0000

    added icons package 'hugeicons.com'

--- src/components/layout/Header/UserMenu.tsx
+++ src/components/layout/Header/UserMenu.tsx
@@ -1,3 +1,11 @@
+import { Menu01Icon } from 'hugeicons-react'
+import { UserIcon } from 'hugeicons-react'
+
 export default function Menu() {
-  return <div></div>
+  return (
+    <div className="flex p-4 border border-neutral-200 rounded-lg gap-4">
+      <Menu01Icon size={24} />
+      <UserIcon size={24} />
+    </div>
+  )
 }
