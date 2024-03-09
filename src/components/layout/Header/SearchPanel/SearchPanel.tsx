5d1c8ab75cabf867316d1d177656b466db9413d7
victorb <hi.victorb@gmail.com>
2024-03-09 12:43:31 +0000

    uninstalled hugeicons package

--- src/components/layout/Header/SearchPanel/SearchPanel.tsx
+++ src/components/layout/Header/SearchPanel/SearchPanel.tsx
@@ -1,9 +1,20 @@
-
-
-
+import styles from './SearchPanel.module.scss'
 
 export default function SearchPanel() {
   return (
-    <div>SearchPanel</div>
+    <div className={styles.container}>
+      <div>
+        <input type="text" placeholder="Search" />
+      </div>
+      <div>
+        <select>
+          <option>Option 1</option>
+          <option>Option 2</option>
+        </select>
+      </div>
+      <div className={styles.search__button}>
+        <p>Search Icon</p>
+      </div>
+    </div>
   )
 }
--- src/components/layout/Header/UserMenu/UserMenu.tsx
+++ src/components/layout/Header/UserMenu/UserMenu.tsx
@@ -1,12 +1,9 @@
-import { Menu01Icon } from 'hugeicons-react'
-import { UserIcon } from 'hugeicons-react'
 import styles from './UserMenu.module.scss'
 
 export default function Menu() {
   return (
     <div className={styles.MenuButton}>
-      <Menu01Icon size={24} />
-      <UserIcon size={24} />
+      <p>User & Menu icons</p>
     </div>
   )
 }
