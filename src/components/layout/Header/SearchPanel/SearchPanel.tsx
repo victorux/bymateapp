3e7bdb4bc13cbe2ea2fa689a9b1f4cc600b324ef
victorb <hi.victorb@gmail.com>
2024-03-10 17:19:38 +0000

    search basic design

--- src/components/layout/Header/SearchPanel/SearchPanel.tsx
+++ src/components/layout/Header/SearchPanel/SearchPanel.tsx
@@ -1,21 +1,35 @@
-import styles from './SearchPanel.module.scss'
 import { MagnifyingGlass } from '@phosphor-icons/react'
+import styles from './SearchPanel.module.scss'
+// import { MagnifyingGlass } from '@phosphor-icons/react'
 
 export default function SearchPanel() {
   return (
-    <div className={styles.container}>
-      <div>
-        <input type="text" placeholder="Search" />
+    <form className={styles.form}>
+      {/* Location */}
+      <div className="flex-grow flex items-center justify-center hover:bg-neutral-100 rounded-full transition">
+        <label htmlFor="search" className="pl-6">
+          <span className={styles.title}>Where</span>
+          <input
+            id="search"
+            type="text"
+            className={styles.search__input}
+            placeholder="Search"
+          />
+        </label>
       </div>
-      <div>
-        <select>
-          <option>Option 1</option>
-          <option>Option 2</option>
-        </select>
+      {/* Divider */}
+      <div className={styles.divider}>
+        <span></span>
       </div>
-      <div className={styles.search__button}>
-        <MagnifyingGlass size={24} color="#ffffff" />
+      {/* Type of Ad */}
+      <div className={styles.what__container}>
+        <span className={styles.title}>What</span>
+        <div>Room</div>
       </div>
-    </div>
+      {/* Button */}
+      <button className={styles.button}>
+        <MagnifyingGlass size={24} color="#fff" />
+      </button>
+    </form>
   )
 }
