28709fba427cc0988c8e83e885c1bae3dbab4735
victorb <hi.victorb@gmail.com>
2024-03-13 13:57:41 +0000

    clicking outside menu closes it.

--- src/components/layout/Header/UserMenu/UserMenu.tsx
+++ src/components/layout/Header/UserMenu/UserMenu.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { List, User } from '@phosphor-icons/react'
 import styles from './UserMenu.module.scss'
 import { COLORS } from '../../../../constants/colors'
@@ -6,13 +6,31 @@ import DesktopMenu from './DesktopMenu'
 
 export default function Menu() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuRef = useRef(null)
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !(menuRef.current as HTMLElement).contains(event.target as Node)
+      ) {
+        setIsMenuOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
   return (
-    <div className="relative">
+    <div className="relative" ref={menuRef}>
       <div className={styles.MenuButton} onClick={toggleMenu}>
         <List size={24} color={COLORS.black} />
         <User size={24} color={COLORS.black} />
