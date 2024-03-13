c5ddf9bb8b5f9fa1417dc8226d69f19143994e39
victorb <hi.victorb@gmail.com>
2024-03-13 14:28:10 +0000

    added rooms page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
-import Footer from "./components/layout/Footer/Footer"
-import Header from "./components/layout/Header/Header"
-import ErrorPage from "./components/pages/error-page";
-import Home from "./components/pages/home/Home"
+import Footer from './components/layout/Footer/Footer'
+import Header from './components/layout/Header/Header'
+import ErrorPage from './components/pages/error-page'
+import Home from './components/pages/home/Home'
+import Rooms from './components/pages/rooms/Rooms'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
   },
-]);
-
+  {
+    path: '/rooms',
+    element: <Rooms />,
+    errorElement: <ErrorPage />,
+  },
+])
 
 function App() {
   return (
     <>
-    <Header />
-    <div>
-      <RouterProvider router={router} />
-    </div>
-    <Footer />
+      <Header />
+      <div>
+        <RouterProvider router={router} />
+      </div>
+      <Footer />
     </>
   )
 }
