006db922120305d9bcd3aa2704ea9f6fe15e1be0
victorb <hi.victorb@gmail.com>
2024-03-18 21:00:12 +0000

    Add thumbails

--- src/components/pages/RoomDetails/RoomDetails.tsx
+++ src/components/pages/RoomDetails/RoomDetails.tsx
@@ -1,6 +1,7 @@
 import { CaretRight, Heart, ShareFat } from '@phosphor-icons/react'
 import { COLORS } from '../../../constants/colors'
 import Button from '../../common/Buttons/Button'
+import Thumbail1 from '../../../assets/property/image01.png'
 
 export default function RoomDetails() {
   return (
@@ -35,36 +36,31 @@ export default function RoomDetails() {
         </div>
       </div>
 
-      <div className="bg-neutral-100 h-[600px] w-full">
-        Property/Room Images
+      <div className="w-full grid grid-cols-4 grid-rows-2 gap-6 h-[640px]">
+        <div
+          className="rounded-md col-span-2 row-span-2 bg-cover bg-center"
+          style={{ backgroundImage: `url(${Thumbail1})` }}
+        />
+        <div
+          className="rounded-md bg-cover bg-center"
+          style={{ backgroundImage: `url(${Thumbail1})` }}
+        />
+        <div
+          className="rounded-md bg-cover bg-center"
+          style={{ backgroundImage: `url(${Thumbail1})` }}
+        />
+        <div
+          className="rounded-md bg-cover bg-center"
+          style={{ backgroundImage: `url(${Thumbail1})` }}
+        />
+        <div
+          className="rounded-md bg-cover bg-center"
+          style={{ backgroundImage: `url(${Thumbail1})` }}
+        />
       </div>
 
       <div className="grid grid-cols-6 gap-10 w-full">
-        <div className="bg-primary-75 col-span-4">
-          <span>Property Info main content:</span>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
-            nihil sit veniam, provident nesciunt minima ad earum error harum
-            debitis culpa in non similique. Dolorum officiis iure architecto
-            laboriosam ipsa! Facilis voluptas ut id eaque beatae accusantium
-            ducimus cupiditate, ullam aspernatur dolores dignissimos, aut sed
-            similique optio quas, sunt consequuntur non? Voluptate nobis atque
-            cum provident dicta esse eveniet. Consectetur. Maiores voluptatum
-            officiis corrupti et praesentium. Nesciunt esse magni earum ad
-            aliquid ipsa quae ipsum harum doloribus deserunt quasi perspiciatis
-            dicta ab commodi totam, voluptatum in illo, delectus nisi. Placeat?
-            Neque tenetur voluptas magni, voluptate omnis dolorem sint eius
-            reiciendis culpa nostrum vitae. Aperiam ut distinctio tempore
-            nostrum error perferendis sapiente, culpa quaerat neque aspernatur,
-            natus ratione harum ex quam. A quidem rem magni reprehenderit rerum?
-            Perspiciatis eligendi dicta explicabo fugit asperiores laudantium
-            incidunt placeat culpa earum sint aspernatur facere, hic deleniti
-            obcaecati molestiae soluta minus! Voluptas nihil deleniti quis.
-            Consectetur officia blanditiis distinctio at? Vero sapiente
-            accusamus qui excepturi aliquid sequi nobis perferendis eum nisi non
-            illo dignissimos minima recusandae repellendus vel
-          </p>
-        </div>
+        <div className="col-span-4"></div>
         <div className="bg-secondary-75 self-start col-span-2">
           <span>Right sidebar:</span>
           <p>
