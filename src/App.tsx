3ee80f357e940ec3d68b4dcff4ad6f7b4c5b4be5
victorb <hi.victorb@gmail.com>
2024-03-24 17:58:51 +0000

    layout structure fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import Footer from './components/layout/Footer/Footer'
-import Header from './components/layout/Header/Header'
 import ErrorPage from './components/pages/error-page'
 import Home from './components/pages/Home/Home'
 import Rooms from './components/pages/Rooms/Rooms'
@@ -9,48 +7,50 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import UserProfile from './components/pages/Profile/UserProfile'
 import MyListings from './components/pages/MyListings/MyListings'
 import CreateListing from './components/pages/CreateListing/CreateListing'
+import Layout from './components/layout/Layout'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/rooms',
-    element: <Rooms />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/rooms/:id',
-    element: <RoomDetails />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/user/:id',
-    element: <UserProfile />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/my-listings',
-    element: <MyListings />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/create-listing',
-    element: <CreateListing />,
-    errorElement: <ErrorPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/rooms',
+        element: <Rooms />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/rooms/:id',
+        element: <RoomDetails />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/user/:id',
+        element: <UserProfile />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/my-listings',
+        element: <MyListings />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/create-listing',
+        element: <CreateListing />,
+        errorElement: <ErrorPage />,
+      },
+    ],
   },
 ])
 
 function App() {
   return (
     <>
-      <Header />
-      <div>
-        <RouterProvider router={router} />
-      </div>
-      <Footer />
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,33 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Footer = () => {
-  const [isContentLong, setIsContentLong] = useState(false);
+  const [isContentLong, setIsContentLong] = useState(false)
+  const location = useLocation()
 
   useEffect(() => {
     const handleResize = () => {
-      setIsContentLong(window.innerHeight < document.body.scrollHeight);
-    };
+      setIsContentLong(window.innerHeight < document.body.scrollHeight)
+    }
 
-    window.addEventListener('resize', handleResize);
-    handleResize(); // Check initially
+    window.addEventListener('resize', handleResize)
+    handleResize() // Check initially
     return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
-  return (
-    <footer className={`absolute bottom-0 left-0 right-0 bg-neutral-75 py-6 ${isContentLong ? 'relative' : 'fixed'}`}>
-      <div className='container flex justify-between'>
-        <div className='flex gap-8'>
-          <a href='#'>support@bymate.com</a>
-          <a href='#'>Privacy & Terms</a>
-          <a href='#'>Testimonials</a>
-          <a href='#'>Sitemap</a>
+  if (location.pathname === '/create-listing') {
+    return null
+  } else {
+    return (
+      <footer
+        className={`absolute bottom-0 left-0 right-0 bg-neutral-75 py-6 ${
+          isContentLong ? 'relative' : 'fixed'
+        }`}
+      >
+        <div className="container flex justify-between">
+          <div className="flex gap-8">
+            <a href="#">support@bymate.com</a>
+            <a href="#">Privacy & Terms</a>
+            <a href="#">Testimonials</a>
+            <a href="#">Sitemap</a>
+          </div>
+          <span>© 2024 bymate LTD.</span>
         </div>
-        <span>© 2024 bymate LTD.</span>
-      </div>
-    </footer>
-  );
-};
+      </footer>
+    )
+  }
+}
 
-export default Footer;
+export default Footer
