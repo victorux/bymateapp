046b3d45c8542cf40aa4d11ea15c997cb2c84772
victorb <hi.victorb@gmail.com>
2024-04-08 22:59:20 +0100

    Add standout and safety buttons

--- src/components/pages/CreateListing/steps/6/Amenities.tsx
+++ src/components/pages/CreateListing/steps/6/Amenities.tsx
@@ -13,6 +13,17 @@ import {
   SprayBottle,
   Car,
   Bread,
+  PottedPlant,
+  DoorOpen,
+  SwimmingPool,
+  PianoKeys,
+  Barbell,
+  DiceTwo,
+  Books,
+  Siren,
+  FirstAid,
+  FireExtinguisher,
+  SecurityCamera,
 } from '@phosphor-icons/react'
 import useFormContext from '../../../../../hooks/useFormContext'
 import { FormData } from '../../../../../types/NewListings'
@@ -37,7 +48,26 @@ const amenitiesData: { [key in keyof FormData['amenities']]: boolean } = {
   cleaningproducts: false,
 }
 
-const icons = {
+const standoutData: { [key in keyof FormData['standoutAmenities']]: boolean } =
+  {
+    patio: false,
+    balcony: false,
+    pool: false,
+    piano: false,
+    gym: false,
+    cleaningservice: false,
+    boardgames: false,
+    books: false,
+  }
+
+const safetyData: { [key in keyof FormData['safetyAmenities']]: boolean } = {
+  smokealarm: false,
+  firstaidkit: false,
+  fireextinguisher: false,
+  cameras: false,
+}
+
+const iconsAmenities = {
   wifi: {
     label: 'Wi-fi',
     icon: <WifiHigh size={32} />,
@@ -100,15 +130,85 @@ const icons = {
   },
 }
 
+const iconsStandout = {
+  patio: {
+    label: 'Garden',
+    icon: <PottedPlant size={32} />,
+  },
+  balcony: {
+    label: 'Balcony',
+    icon: <DoorOpen size={32} />,
+  },
+  pool: {
+    label: 'Pool',
+    icon: <SwimmingPool size={32} />,
+  },
+  piano: {
+    label: 'Piano',
+    icon: <PianoKeys size={32} />,
+  },
+  gym: {
+    label: 'Gym',
+    icon: <Barbell size={32} />,
+  },
+  cleaningservice: {
+    label: 'Cleaning service',
+    icon: <Chair size={32} />,
+  },
+  boardgames: {
+    label: 'Board games',
+    icon: <DiceTwo size={32} />,
+  },
+  books: {
+    label: 'Books',
+    icon: <Books size={32} />,
+  },
+}
+
+const iconsSafety = {
+  smokealarm: {
+    label: 'Smoke alarm',
+    icon: <Siren size={32} />,
+  },
+  firstaidkit: {
+    label: 'First aid kit',
+    icon: <FirstAid size={32} />,
+  },
+  fireextinguisher: {
+    label: 'Fire extinguisher',
+    icon: <FireExtinguisher size={32} />,
+  },
+  cameras: {
+    label: 'Cameras',
+    icon: <SecurityCamera size={32} />,
+  },
+}
+
 const Amenities = () => {
   const { updateFormData, formData } = useFormContext()
 
-  const handleClick = (key: keyof FormData['amenities']) => {
+  const handleClickAmmenities = (key: keyof FormData['amenities']) => {
     updateFormData('amenities', {
       ...formData?.amenities,
       [key]: !formData?.amenities[key],
     } as FormData['amenities'])
     console.log(formData?.amenities[key])
+  }
+
+  const handleClickStandout = (key: keyof FormData['standoutAmenities']) => {
+    updateFormData('standoutAmenities', {
+      ...formData?.standoutAmenities,
+      [key]: !formData?.standoutAmenities[key],
+    } as FormData['standoutAmenities'])
+    console.log(formData?.standoutAmenities[key])
+  }
+
+  const handleClickSafety = (key: keyof FormData['safetyAmenities']) => {
+    updateFormData('safetyAmenities', {
+      ...formData?.safetyAmenities,
+      [key]: !formData?.safetyAmenities[key],
+    } as FormData['safetyAmenities'])
+    console.log(formData?.safetyAmenities[key])
   }
 
   return (
@@ -119,17 +219,53 @@ const Amenities = () => {
           {Object.entries(amenitiesData).map(([key]) => (
             <CustomCheckButton
               key={key}
-              icon={icons[key as keyof typeof icons].icon}
+              icon={iconsAmenities[key as keyof typeof iconsAmenities].icon}
               selected={
                 formData?.amenities[key as keyof FormData['amenities']] ?? false
               }
-              label={icons[key as keyof typeof icons].label}
-              onClick={() => handleClick(key as keyof FormData['amenities'])}
+              label={iconsAmenities[key as keyof typeof iconsAmenities].label}
+              onClick={() =>
+                handleClickAmmenities(key as keyof FormData['amenities'])
+              }
             />
           ))}
         </div>
         <Heading>Do you have any standout amenities?</Heading>
+        <div className="grid grid-cols-3 max-w-[700px] gap-4">
+          {Object.entries(standoutData).map(([key]) => (
+            <CustomCheckButton
+              key={key}
+              icon={iconsStandout[key as keyof typeof iconsStandout].icon}
+              selected={
+                formData?.standoutAmenities[
+                  key as keyof FormData['standoutAmenities']
+                ] ?? false
+              }
+              label={iconsStandout[key as keyof typeof iconsStandout].label}
+              onClick={() =>
+                handleClickStandout(key as keyof FormData['standoutAmenities'])
+              }
+            />
+          ))}
+        </div>
         <Heading>Do you have any of these safety items?</Heading>
+        <div className="grid grid-cols-3 max-w-[700px] gap-4">
+          {Object.entries(safetyData).map(([key]) => (
+            <CustomCheckButton
+              key={key}
+              icon={iconsSafety[key as keyof typeof iconsSafety].icon}
+              selected={
+                formData?.safetyAmenities[
+                  key as keyof FormData['safetyAmenities']
+                ] ?? false
+              }
+              label={iconsSafety[key as keyof typeof iconsSafety].label}
+              onClick={() =>
+                handleClickSafety(key as keyof FormData['safetyAmenities'])
+              }
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
