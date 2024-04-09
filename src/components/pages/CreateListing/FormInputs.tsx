eaa3917ae1f9ef7be4740c9ee4b34badc701e27f
victorb <hi.victorb@gmail.com>
2024-04-09 22:54:09 +0100

    Add title and description steps

--- src/components/pages/CreateListing/FormInputs.tsx
+++ src/components/pages/CreateListing/FormInputs.tsx
@@ -1,6 +1,7 @@
 import useFormContext from '../../../hooks/useFormContext'
 import Step1About from './steps/0/Step1About'
 import PropertyType from './steps/1/PropertyType'
+import GiveDescription from './steps/10/GiveDescription'
 import SpaceType from './steps/2/SpaceType'
 import EnterAddress from './steps/3/EnterAddress'
 import Baths from './steps/4/Baths'
@@ -8,6 +9,7 @@ import Step2Banner from './steps/5/Step2Banner'
 import Amenities from './steps/6/Amenities'
 import UploadPhotos from './steps/7/UploadPhotos'
 import Step3Finish from './steps/8/Step3Finish'
+import GiveTitle from './steps/9/GiveTitle'
 
 const FormInputs = () => {
   const { page } = useFormContext()
@@ -26,6 +28,8 @@ const FormInputs = () => {
     6: <Amenities />,
     7: <UploadPhotos />,
     8: <Step3Finish />,
+    9: <GiveTitle />,
+    10: <GiveDescription />,
   }
 
   return <div>{displayStep[page]}</div>
