ac784e108b99bf151560e5cdf1ba2c5109cb7252
victorb <hi.victorb@gmail.com>
2024-04-11 10:30:41 +0100

    git commit -m "Separate frontend code into its own directory"

--- src/constants/colors.ts
+++
@@ -1,3 +0,0 @@
-export const COLORS = {
-  black: '#181B2A',
-}
--- src/hooks/useFormContext.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import FormContext from '../context/FormContext'
-
-const useFormContext = () => {
-  return useContext(FormContext)
-}
-
-export default useFormContext
