e99827ba1e59c4d35d5d54239b07405ae15edabd
victorb <hi.victorb@gmail.com>
2024-04-11 20:45:44 +0100

    testing db

--- backend/app.js
+++ backend/app.js
@@ -9,20 +9,43 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-connection.addListener("error", (err) => {
+db.addListener("error", (err) => {
   console.log(err);
 });
 
 // create
+app.post("/books", (req, res) => {
+  const { title, description, cover } = req.body;
+  db.query(
+    "INSERT INTO books (title, description, cover) VALUES (?, ?, ?)",
+    [title, description, cover],
+    (err, data) => {
+      if (err) return res.json(err);
+      return res.json("Book added successfully");
+    }
+  );
+});
 
 // read
+
+app.get("/", (req, res) => {
+  res.json("Hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 // update
 
