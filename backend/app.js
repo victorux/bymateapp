8cb02b3dc06771d813cbbc45d09703fb217136b8
victorb <hi.victorb@gmail.com>
2024-04-11 12:30:29 +0100

    Connect to database

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
+const mysql = require("mysql2");
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.addListener("error", (err) => {
+  console.log(err);
+});
 
 // create
 
@@ -14,6 +26,6 @@ app.use(express.urlencoded({ extended: false }));
 
 // update
 
-delete app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
