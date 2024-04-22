d4459d611e046af84ae52560a768c0e4d1f894d0
victorb <hi.victorb@gmail.com>
2024-04-22 14:11:46 +0100

    address update

--- backend/passport.js
+++ backend/passport.js
@@ -9,7 +9,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/api/auth/google/callback",
+      callbackURL:
+        "https://bymate-server-874ca7821918.herokuapp.com/api/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       // Check if the user exists in the database
