fde82c850e2529bf0355faa9a92e9a6e699e3eea
victorb <hi.victorb@gmail.com>
2024-04-23 13:07:55 +0100

    cookie requierd

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -13,7 +13,7 @@ export const loginSuccess = async (req, res) => {
       success: true,
       message: "successfull",
       user: req.user,
-      //   cookies: req.cookies
+      cookies: req.cookies,
     });
   }
 };
